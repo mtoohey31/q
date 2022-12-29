5ee6252ea8a12c586bac89c5065778cada52cfbf
Matthew Toohey <contact@mtoohey.com>
2022-12-29 11:31:29 -0500

    fix(tui): clear gap between info and progress

--- internal/tui/bottom.go
+++ internal/tui/bottom.go
@@ -11,6 +11,8 @@ func (t *tui) drawBottom(new, old image.Image) error {
 		return fmt.Errorf("failed to draw cover: %w", err)
 	}
 
+	t.clear(image.Rect(stopX, t.bottomR.Min.Y, stopX+1, t.bottomR.Max.Y))
+
 	infoMinX := stopX + 1
 	t.infoMaxR = image.Rect(infoMinX, t.bottomR.Min.Y, infoMinX+((t.bottomR.Max.X-infoMinX)/3), t.bottomR.Max.Y-1)
 
