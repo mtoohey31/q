e02b841e9bce41ef1a75f837f05d65ebf854de78
Matthew Toohey <contact@mtoohey.com>
2022-12-29 00:18:13 -0500

    build: don't use flake inputs for errcheck and gow srcs

--- flake.nix
+++ flake.nix
@@ -4,17 +4,9 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    errcheck-src = {
-      url = "github:kisielk/errcheck";
-      flake = false;
-    };
-    gow-src = {
-      url = "github:mitranim/gow";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, utils, errcheck-src, gow-src }: {
+  outputs = { self, nixpkgs, utils }: {
     overlays.default = final: _: {
       q = final.buildGoModule {
         pname = "q";
@@ -29,16 +21,26 @@
     {
       overlays = [
         (final: _: {
-          errcheck = final.buildGoModule {
+          errcheck = final.buildGoModule rec {
             pname = "errcheck";
-            version = errcheck-src.shortRev;
-            src = errcheck-src;
+            version = builtins.substring 0 7 src.rev;
+            src = final.fetchFromGitHub {
+              owner = "kisielk";
+              repo = pname;
+              rev = "3faf0bc8574bd40c9635d7910b25dd81349f6e89";
+              sha256 = "lx1kbRyL9OJzTxClIej/FisfVRh2VG98HGOBuF359LI=";
+            };
             vendorSha256 = "96+927gNuUMovR4Ru/8BwsgEByNq2EPX7wXWS7+kSL8=";
           };
-          gow = final.buildGoModule {
+          gow = final.buildGoModule rec {
             pname = "gow";
-            version = gow-src.shortRev;
-            src = gow-src;
+            version = builtins.substring 0 7 src.rev;
+            src = final.fetchFromGitHub {
+              owner = "mitranim";
+              repo = pname;
+              rev = "a5bfab26a0e42ee646f0969ac3397e80e5e3b1df";
+              sha256 = "vlIbVoAxeeQ1SB8FmSAfQ35fX6f+/VGZmrPDdA3HTvs=";
+            };
             vendorSha256 = "o6KltbjmAN2w9LMeS9oozB0qz9tSMYmdDW3CwUNChzA=";
           };
         })
