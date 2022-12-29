e5bf2bff6d5745a438a6e2a2709f396b9b248e07
Matthew Toohey <contact@mtoohey.com>
2022-12-28 19:52:19 -0500

    fix(tui): start server when --unix-socket is not provided

--- internal/tui/cmd.go
+++ internal/tui/cmd.go
@@ -29,69 +29,71 @@ func (c Cmd) Run() (err error) {
 	var wg sync.WaitGroup
 	defer wg.Wait()
 
+	// start out assuming we will have to start our own server
+	startServer := true
 	if c.UnixSocket != "" {
 		_, err = os.Stat(c.UnixSocket)
-		if err != nil && !errors.Is(err, os.ErrNotExist) {
+		if err == nil {
+			// only change this assumption if the unix socket flag was
+			// provided and the socket already exists
+			startServer = false
+		} else if !errors.Is(err, os.ErrNotExist) {
 			// we got an unexpected error, bail
 			return fmt.Errorf("unexpected error while checking whether socket exists: %w", err)
 		}
+	}
 
+	if startServer {
+		out := io.Discard
+		if c.ServerLogPath != "" {
+			f, err := os.OpenFile(c.ServerLogPath, os.O_RDWR|os.O_CREATE, 0o600)
+			if err != nil {
+				return fmt.Errorf("failed to create server log file: %w", err)
+			}
+			out = f
+		}
+
+		s, err := server.NewServer(c.Cmd, log.New(out, "", log.LstdFlags))
 		if err != nil {
-			// then errors.Is(err, os.ErrNotExist), since otherwise we would've
-			// hit the first condition. in this case, the socket doesn't exist,
-			// so we should create our own server which will bind to it
-			out := io.Discard
-			if c.ServerLogPath != "" {
-				f, err := os.OpenFile(c.ServerLogPath, os.O_RDWR|os.O_CREATE, 0o600)
-				if err != nil {
-					return fmt.Errorf("failed to create server log file: %w", err)
-				}
-				out = f
+			return fmt.Errorf("failed to start server: %w", err)
+		}
+
+		// make sure we only close once
+		var closeOnce sync.Once
+		doClose := func() {
+			closeErr := s.Close()
+			if err == nil {
+				err = closeErr
 			}
+		}
+		defer closeOnce.Do(doClose)
 
-			s, err := server.NewServer(c.Cmd, log.New(out, "", log.LstdFlags))
-			if err != nil {
-				return fmt.Errorf("failed to start server: %w", err)
+		// ensure the server's resources get cleaned up even if we get killed
+		sigCh := make(chan os.Signal, 2)
+		go func() {
+			<-sigCh
+			closeOnce.Do(doClose)
+		}()
+		signal.Notify(sigCh, os.Interrupt)
+		defer signal.Stop(sigCh)
+
+		// start the server
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			serveErr := s.Serve()
+
+			if err == nil {
+				err = serveErr
 			}
+		}()
 
-			// make sure we only close once
-			var closeOnce sync.Once
-			doClose := func() {
-				closeErr := s.Close()
-				if err == nil {
-					err = closeErr
-				}
-			}
-			defer closeOnce.Do(doClose)
-
-			// ensure the server's resources get cleaned up even if we get killed
-			sigCh := make(chan os.Signal, 2)
-			go func() {
-				<-sigCh
-				closeOnce.Do(doClose)
-			}()
-			signal.Notify(sigCh, os.Interrupt)
-			defer signal.Stop(sigCh)
-
-			// start the server
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				serveErr := s.Serve()
-
-				if err == nil {
-					err = serveErr
-				}
-			}()
-
-			conn = s.ChannelConn()
-		} else {
-			// then the socket already exists, so let's try and connect to it
-
-			conn, err = unixsocketconn.NewUnixSocketClientConn(c.UnixSocket)
-			if err != nil {
-				return fmt.Errorf("failed to connect to existing socket: %w", err)
-			}
+		conn = s.ChannelConn()
+	} else {
+		// try to connect to the existing socket
+		conn, err = unixsocketconn.NewUnixSocketClientConn(c.UnixSocket)
+		if err != nil {
+			return fmt.Errorf("failed to connect to existing socket: %w", err)
 		}
 	}
 
